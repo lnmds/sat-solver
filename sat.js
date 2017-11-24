db5cc829dfcabefaf08acb8874599972a706df18
Luna Mendes <lkmnds@gmail.com>
2017-11-23 22:09:02 -0300

    add implementations of readFormula and readVariables

--- sat.js
+++ sat.js
@@ -7,8 +7,10 @@
  *
  * Licensed under MIT
  */
+var fs = require('fs')
+
 exports.solve = function(fileName) {
-    let formula = propsat.readFormula(fileName)
+    let formula = readFormula(fileName)
     let result = doSolve(formula.clauses, formula.variables)
     return result // two fields: isSat and satisfyingAssignment
 }
@@ -21,7 +23,8 @@ function nextAssignment(currentAssignment) {
 
 function doSolve(clauses, assignment) {
     let isSat = false
-    while ((!isSat) && /* must check whether this is the last assignment or not*/ ) {
+    //while ((!isSat) && /* must check whether this is the last assignment or not*/ ) {
+    while ((!isSat)) {
 	// does this assignment satisfy the formula? If so, make isSat true. 
 
 	// if not, get the next assignment and try again. 
@@ -36,13 +39,84 @@ function doSolve(clauses, assignment) {
     return result
 }
 
+function readProblemSpecification(text, clauses, variables){
+
+    // already allocate 2 spots
+    let problemData = [undefined, undefined]
+    
+    for(var i = 0; i < text.length; i++){
+	let line = text[i]
+
+	// ignore blank lines lol
+	if(!line.length) continue;
+
+	let prefix = line.charAt(0)
+
+	if(prefix == 'p') {
+	    // TODO: parse problem
+	}
+    }
+
+    if(!problemData[0]){
+	return false;
+    } else {
+	return true;
+    }
+
+}
+
+function readClauses(text)
+{
+    let clauses = []
+
+    for(var i = 0; i < text.length; i++){
+	let line = text[i]
+
+	// ignore blank lines lol
+	if(!line.length) continue;
+
+	let prefix = line.charAt(0)
+
+	if(prefix != 'c' && prefix != 'p') {
+	    clauses = clauses.concat(line)
+	}
+    }
+
+    return clauses
+}
+
+function readVariables(clauses) {
+    let variables = []
+
+    for(var i = 0; i < clauses.length; i++) {
+	let clause = clauses[i]
+
+	let vars = clause.split(' ')
+
+	// this is really terrible, but what can I do?
+	for(var j = 0; j < vars.length; j++) {
+	    let variable = vars[j]
+	    let variableAsInt = parseInt(variable)
+	    variableAsInt = Math.abs(variableAsInt)
+	    
+	    if(!variables.includes(variableAsInt)){
+		variables = variables.concat([variableAsInt])
+	    }
+	}
+    }
+
+    return variables
+}
+
 function readFormula(fileName) {
-    // To read the file, it is possible to use the 'fs' module. 
-    // Use function readFileSync and not readFile. 
-    // First read the lines of text of the file and only afterward use the auxiliary functions.
-    let text // = ...  //  an array containing lines of text extracted from the file. 
+    let data = fs.readFileSync(fileName, {'encoding': 'utf-8'})
+    let text = data.split('\n')
+
     let clauses = readClauses(text)
+    console.log('clauses', clauses)
+
     let variables = readVariables(clauses)
+    console.log('variables', variables)
     
     // In the following line, text is passed as an argument so that the function
     // is able to extract the problem specification.
@@ -55,3 +129,11 @@ function readFormula(fileName) {
     }
     return result
 }
+
+function main(){
+    // so it executes as a script
+    let r = readFormula(process.argv[2])
+    console.log(r)
+}
+
+main()
