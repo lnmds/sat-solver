fae85cc3a3a6041428a696b5eb82ced63b873ddc
Luna Mendes <lkmnds@gmail.com>
2017-11-25 15:52:28 -0300

    sat: a lot of things
     - figured out you can do the permutation of available
     assignments by converting a number to int
    
     - add a proper implementation of checkProblemSpecification
     - fix readClauses because of the "0" clause splitter

--- sat.js
+++ sat.js
@@ -12,31 +12,92 @@ exports.solve = function(fileName) {
     return result; // two fields: isSat and satisfyingAssignment
 };
 
+function toBinary(num, state){
+    // if(num == 0 && !state) return '0';
+    if(num == 0){
+        return state;
+    }
+
+    let digit = num % 2;
+    let next = Math.floor(num / 2);
+
+    state = digit + state;
+
+    return toBinary(next, state);
+}
+
+function toAssignment(num, len) {
+    let asBin = toBinary(num, '');
+    console.log(asBin);
+    let assignment = [];
+
+    for(const b of asBin){
+        if(b == '1'){
+            assignment.push(true);
+        }else{
+            assignment.push(false);
+        }    
+    }
+
+    console.log(assignment);
+
+    while(assignment.length < len){
+        assignment.unshift(false);
+    }
+
+    return assignment;
+}
+
 // Receives the current assignment and produces the next one
-function nextAssignment(currentAssignment) {
-    // TODO: implement
-    return [];
+// Yes, I have the shittiest gambiarra(tm) for this.
+function nextAssignment(oldState) {
+    return oldState + 1;
+}
+
+// Apply an assignment of vars to the formula
+function apply(state, variables, clauses) {
+    let assignment = toAssignment(state, variables.length);
+    console.log(assignment, variables, clauses);
+
+    let clauseResults = [];
+    for(const clause of clauses){
+        
+    }
+
+    let result = clauseResults[0];
+    for(const cResult of clauseResults.slice(1)){
+        
+    }
+    return true;
 }
 
 // solve SAT for the given formula(in clauses, array)
 // with the initial state of assignemnt
-function doSolve(clauses, initialAssignment) {
+function doSolve(clauses, variables) {
     let isSat = false;
-    let assignemnt = initialAssignment;
 
-    // TODO: check if we are in the last assingment
-    while (!isSat) {
-        // TODO: evaluate the assingment with the clauses
-        // set the result of evaluation to isSat, plus break the loop
+    let vars = variables.length;
+    let maxState = Math.pow(2, vars);
+    let currentState = 0;
+
+    console.log(currentState, maxState);
+ 
+    while (!isSat && currentState < maxState) {
+ 
+        let result = apply(currentState, variables, clauses);
+        if(result) {
+            isSat = true;
+            break;
+        }
 
         // Continue until we finish all available assignemnts
-        assignment = nextAssignment(assignment);
+        currentState = nextAssignment(currentState);
     }
 
     let result = {isSat, satisfyingAssignment: null};
 
     if (isSat) {
-        result.satisfyingAssignment = assignment;
+        result.satisfyingAssignment = toAssignment(currentState, vars);
     }
 
     return result;
@@ -44,39 +105,46 @@ function doSolve(clauses, initialAssignment) {
 
 // Check if the problem spec is actually valid
 // given our parsing
-function readProblemSpecification(text, clauses, variables){
-    let problemData = [];
+function checkProblemSpecification(text, clauses, variables){
+    let total_vars;
+    let total_clauses;
     
     for(const line of text){
         if(!line) continue;
 
         let prefix = line.charAt(0);
         if(prefix == 'p') {
-            // TODO: parse problem
+            let _problem = line.split(' ');
+            total_vars = _problem[2];
+            total_clauses = _problem[3];
         }
     }
 
-    // TODO: better way
-    if(!problemData[0]){
-        return false;
-    } else {
-        return true;
-    }
-
+    return (clauses.length == total_clauses) &&
+        (variables.length == total_vars);
 }
 
 // Get the clauses from the CNF text
 function readClauses(text) {
     let clauses = [];
+    let current = [];
 
     for(const line of text){
         if(!line) continue;
-
         let prefix = line.charAt(0);
 
         // ignore comments and the problem description
-        if(prefix != 'c' && prefix != 'p') {
-            clauses.push(line);
+        if(prefix == 'c' || prefix == 'p') {
+            continue;
+        }
+
+        for(const varb of line.split(' ')){
+            if(varb == '0'){
+                clauses.push(current);
+                current = [];
+            } else {
+                current.push(varb);
+            }
         }
     }
 
@@ -88,7 +156,7 @@ function readVariables(clauses) {
     let variables = [];
     
     clauses.map((clause) => {
-        clause.split(' ').map((variable) => {
+        clause.map((variable) => {
             variable = Math.abs(parseInt(variable));
             variable = variable.toString();
 
@@ -102,30 +170,32 @@ function readVariables(clauses) {
 }
 
 function readFormula(fileName) {
+    // Why encoding... WHY!?!?!?!
     let data = fs.readFileSync(fileName, {'encoding': 'utf-8'});
     let text = data.split('\n');
 
     let clauses = readClauses(text);
-    console.log('clauses', clauses);
-
     let variables = readVariables(clauses);
-    console.log('variables', variables);
     
     // Check if the problem actually makes sense
     // given its definition and the data we've read.
     let specOk = checkProblemSpecification(text, clauses, variables);
 
     let result = {
+        invalid: true,
         clauses: [],
         variables: []
     };
 
     if (specOk) {
         result = {
+            invalid: false,
             clauses,
             variables,
         };
     }
+
+    console.log(result);
 
     return result;
 }
